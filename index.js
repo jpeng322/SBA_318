7143762eff37f225e38eec6ebe92e29f3585f0c3
Jacky <jackypengsmail@gmail.com>
2023-11-09 15:03:01 -0500

    Create first  test routes

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+  res.send("This is the home page. Server is available");
+});
+
+app.post("/api", (req, res) => {
+  res.send("Test for api post route");
+});
+
+app.put("/api", (req, res) => {
+  res.send("Test for api put route");
+});
+
 app.listen(port, () => {
   console.log(`Server is listening on port: ${port}`);
 });
