7b106d63850778fdcdb97b78a018c9b6dd1c5bc2
Jacky <jackypengsmail@gmail.com>
2023-11-11 23:17:23 -0500

    Create user routes

---
+++ functions/functions.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+
+function updateFile(path, file) {
+  fs.writeFileSync(path, "module.exports = " + JSON.stringify(file));
+}
+
+module.exports = {
+  updateFile,
+};
--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const users = require("./data/users.js");
+const fs = require("fs");
+const bodyParser = require("body-parser");
+const { updateFile } = require("./functions/functions.js");
 
+//middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ extended: true }));
+
+// app.use(express.json());
 app.get("/", (req, res) => {
   res.send("This is the home page. Server is available");
 });
@@ -12,6 +21,50 @@ app.post("/api", (req, res) => {
 
 app.put("/api", (req, res) => {
   res.send("Test for api put route");
+});
+
+app.post("/api/users", (req, res) => {
+  console.log(req.body);
+  const { fname, lname, username, email } = req.body;
+  if (fname && lname && username && email) {
+    if (users.find((user) => user.email === email)) {
+      res.json({ error: "Email is already in use." });
+      return;
+    }
+
+    if (users.find((user) => user.username === username)) {
+      res.json({ error: "Username is already taken." });
+      return;
+    }
+    const user = {
+      id: users[users.length - 1].id + 1 || 1,
+      fname,
+      lname,
+      username,
+      email,
+    };
+    users.push(user);
+    updateFile("./data/users.js", users);
+    res.status(201).json({
+      user,
+      success: true,
+    });
+  } else {
+    res.status(400).json({ error: "Insufficient Data" });
+  }
+});
+
+app.get("/api/users", (req, res) => {
+  res.json(users);
+});
+
+app.get("/api/users/:id", (req, res) => {
+  const user = users.find((user) => user.id == req.params.id);
+  if (user) {
+    res.status(200).json(user);
+  } else {
+    res.status(400).json({ error: "Insufficient Data" });
+  }
 });
 
 app.listen(port, () => {
